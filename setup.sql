f150821f1ae93836c203330a41e7b5862d8d34de
Elliot Schumacher <Elliot.l.schumacher.code@gmail.com>
2021-03-29 14:34:06 -0700

    updated SQL structure

--- setup.sql
+++ setup.sql
@@ -6,8 +6,11 @@ USE tempSensors;
 DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Users(
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(32) NOT NULL UNIQUE,
+  error_int INT DEFAULT 1800000,
+  temp_check_int INT DEFAULT 30000,
+  log_int INT DEFAULT 1800000,
   PRIMARY KEY (id)
 );
 
@@ -16,6 +19,9 @@ DROP TABLE IF EXISTS Nodes;
 CREATE TABLE Nodes(
     id INT NOT NULL AUTO_INCREMENT,
     user_id INT NOT NULL,
+    order_num INT NOT NULL,
+    name VARCHAR(32) NOT NULL,
+    error_leds BOOLEAN DEFAULT TRUE,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES Users(id)
 );
@@ -25,10 +31,12 @@ DROP TABLE IF EXISTS Sensors;
 CREATE TABLE Sensors(
     id INT NOT NULL AUTO_INCREMENT,
     node_id INT NOT NULL,
-    relative_id INT NOT NULL,
+    order_num INT NOT NULL,
     name VARCHAR(32) NOT NULL,
-    upper_bound INT,
-    lower_bound INT,
+    upper_bound INT NOT NULL,
+    lower_bound INT NOT NULL,
+    address VARCHAR(64),
+    temperature INT,
     PRIMARY KEY (id),
     FOREIGN KEY (node_id) REFERENCES Nodes(id)
 
